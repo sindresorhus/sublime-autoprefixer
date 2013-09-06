66413dd40e1c27b542df8457051f9c930e3523f6
Sindre Sorhus <sindresorhus@gmail.com>
2013-09-06 13:46:03 +0200

    Use `css` instead of `cssbeautify` and upgrade autoprefixer
    CSSBeautify is broken.

--- autoprefixer.js
+++ autoprefixer.js
@@ -1,6 +1,6 @@
 'use strict';
 var autoprefixer = require('autoprefixer');
-var cssbeautify = require('cssbeautify');
+var css = require('css');
 var detectIndent = require('detect-indent');
 var str = '';
 
@@ -14,9 +14,8 @@ process.stdin.on('data', function (data) {
 process.stdin.on('end', function () {
 	var browsers = process.argv[2].split(',');
 	var prefixed = autoprefixer.apply(this, browsers).compile(str);
-	var beautified = cssbeautify(prefixed, {
-		indent: detectIndent(str) || '  ',
-		autosemicolon: true
+	var beautified = css.stringify(css.parse(prefixed), {
+		indent: detectIndent(str) || '    '
 	});
 
 	process.stdout.write(beautified);
