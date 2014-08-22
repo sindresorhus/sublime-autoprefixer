5bea4a21ff1d22204cbefb50f61a90b4479f9abd
Sindre Sorhus <sindresorhus@gmail.com>
2014-08-23 00:24:43 +0200

    use autoprefixer-core 3.0.0

--- autoprefixer.js
+++ autoprefixer.js
@@ -1,20 +1,22 @@
 'use strict';
 var stdin = require('get-stdin');
-var autoprefixer = require('autoprefixer');
+var autoprefixer = require('autoprefixer-core');
 
 stdin(function (data) {
 	var opts = JSON.parse(process.argv[2]);
 
 	try {
-		var css = autoprefixer(opts.browsers, opts).process(data, {safe: true}).css;
+		var css = autoprefixer(opts).process(data, {safe: true}).css;
 		process.stdout.write(css);
 	} catch (err) {
 		if (/Unclosed block/.test(err.message)) {
-			return console.error('Couldn\'t find any valid CSS rules. You can\'t select properties. Select a whole rule and try again.');
+			console.error('Couldn\'t find any valid CSS rules. You can\'t select properties. Select a whole rule and try again.');
+			return;
 		}
 
 		if (err.name === 'TypeError') {
-			return console.error('Invalid CSS.');
+			console.error('Invalid CSS');
+			return
 		}
 
 		throw err;
