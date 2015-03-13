42ec33274f332c2442bc0edb46c5ab01cbcbda9f
Sindre Sorhus <sindresorhus@gmail.com>
2015-03-13 20:04:07 +0800

    improve Windows compatibility

--- node_bridge.py
+++ node_bridge.py
@@ -1,20 +1,24 @@
 import os
 import platform
-from subprocess import Popen, PIPE
+import subprocess
 
 IS_OSX = platform.system() == 'Darwin'
 IS_WINDOWS = platform.system() == 'Windows'
 
 def node_bridge(data, bin, args=[]):
 	env = None
+	startupinfo = None
 	if IS_OSX:
 		# GUI apps in OS X doesn't contain .bashrc/.zshrc set paths
 		env = os.environ.copy()
 		env['PATH'] += ':/usr/local/bin'
+	if IS_WINDOWS:
+		startupinfo = subprocess.STARTUPINFO()
+		startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
 	try:
-		p = Popen(['node', bin] + args,
-			stdout=PIPE, stdin=PIPE, stderr=PIPE,
-			env=env, shell=IS_WINDOWS)
+		p = subprocess.Popen(['node', bin] + args,
+			stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE,
+			env=env, startupinfo=startupinfo)
 	except OSError:
 		raise Exception('Couldn\'t find Node.js. Make sure it\'s in your $PATH by running `node -v` in your command-line.')
 	stdout, stderr = p.communicate(input=data.encode('utf-8'))
